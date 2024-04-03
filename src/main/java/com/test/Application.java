80137e041fe9b3d3017cccfe5830bf81c8da6435
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-03 11:52:56 +0800

    Update Application.java

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -3,7 +3,9 @@ package com.test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 @MapperScan("com.test.mapper")
 public class Application {
