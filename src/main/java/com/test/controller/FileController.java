6fc7c88f3b32b846ad0467ed43b568eb12d89467
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-09 21:27:15 +0800

    download

--- src/main/java/com/test/controller/FileController.java
+++ src/main/java/com/test/controller/FileController.java
@@ -1,12 +1,18 @@
 package com.test.controller;
 
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -41,7 +47,7 @@ public class FileController {
         // 指定 转移目标文件
         File target = new File(
                 ResourceUtils.getURL("classpath:").getPath() +
-                        "static/uploads/" +
+                        "uploads/" +
                         filename );
 
         // 将 上传文件 从临时目录 转移到 目标文件夹
@@ -50,29 +56,29 @@ public class FileController {
         return null;
     }
 
-//    @GetMapping("/download")
-//    public ResponseEntity<byte[]> download( String filename ) throws IOException {
-//
-//        // 指定要下载的文件
-//        File file = new File(
-//                ResourceUtils.getURL("classpath:").getPath() +
-//                "downloads/" + filename
-//        );
-//
-//        // 实例化 响应报文头 对象
-//        HttpHeaders headers = new HttpHeaders();
-//        // 设置 浏览器下载文件时的文件名
-//        headers.setContentDispositionFormData( "attachment" , filename );
-//        // 设置 浏览器解析响应报文的方式 以 输出流 的方式解析
-//        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-//
-//        // 返回响应报文
-//        return new ResponseEntity<byte[]>(
-//                FileUtils.readFileToByteArray( file ),  // 响应报文体
-//                headers,                                // 响应报文头
-//                HttpStatus.CREATED                      // 响应状态
-//        );
-//
-//    }
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> download( String filename ) throws IOException {
+
+        // 指定要下载的文件
+        File file = new File(
+                ResourceUtils.getURL("classpath:").getPath() +
+                        "downloads/" + filename
+        );
+
+        // 实例化 响应报文头 对象
+        HttpHeaders headers = new HttpHeaders();
+        // 设置 浏览器下载文件时的文件名
+        headers.setContentDispositionFormData( "attachment" , filename );
+        // 设置 浏览器解析响应报文的方式 以 输出流 的方式解析
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+        // 返回响应报文
+        return new ResponseEntity<byte[]>(
+                FileUtils.readFileToByteArray( file ),  // 响应报文体
+                headers,                                // 响应报文头
+                HttpStatus.CREATED                      // 响应状态
+        );
+
+    }
 
 }
