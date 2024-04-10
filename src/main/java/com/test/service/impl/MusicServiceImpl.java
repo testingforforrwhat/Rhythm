1ada61e40ec43118b366633ca81dca18e6b3c08f
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-10 10:39:19 +0800

    Update MusicServiceImpl.java

--- src/main/java/com/test/service/impl/MusicServiceImpl.java
+++ src/main/java/com/test/service/impl/MusicServiceImpl.java
@@ -1,12 +1,19 @@
 package com.test.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.test.bean.bo.MusicAddBo;
 import com.test.bean.bo.MusicUpdateBo;
 import com.test.bean.po.Music;
+import com.test.bean.po.MusicCategories;
+import com.test.mapper.MusicCategoriesMapper;
 import com.test.service.MusicService;
 import com.test.mapper.MusicMapper;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
 * @author 23194
@@ -17,6 +24,11 @@ import org.springframework.stereotype.Service;
 public class MusicServiceImpl extends ServiceImpl<MusicMapper, Music>
     implements MusicService{
 
+
+    // 依赖项
+    @Resource
+    private MusicMapper musicMapper;
+
     /**
      * 根据music_id查询对应信息
      *
@@ -25,7 +37,7 @@ public class MusicServiceImpl extends ServiceImpl<MusicMapper, Music>
      */
     @Override
     public Object selectByMusicId(Integer musicId) {
-        return null;
+        return musicMapper.selectById( musicId );
     }
 
     /**
@@ -35,7 +47,7 @@ public class MusicServiceImpl extends ServiceImpl<MusicMapper, Music>
      */
     @Override
     public Object listAll() {
-        return null;
+        return musicMapper.selectList( null );
     }
 
     /**
@@ -46,7 +58,19 @@ public class MusicServiceImpl extends ServiceImpl<MusicMapper, Music>
      */
     @Override
     public boolean add(MusicAddBo musicAddBo) {
-        return false;
+        // 创建用于添加的Music对象
+        Music music = new Music();
+        // 借组属性拷贝工具将bo中的同名的属性值 赋给 music
+        musicAddBo.setTitle( musicAddBo.getTitle() );
+        musicAddBo.setArtist( musicAddBo.getArtist() );
+        musicAddBo.setAlbum( musicAddBo.getAlbum() );
+        musicAddBo.setCategoryId( musicAddBo.getCategoryId() );
+        System.out.println( musicAddBo.getTitle() );
+
+
+        BeanUtil.copyProperties(musicAddBo,music);
+        System.out.println( music );
+        return musicMapper.insert( music ) > 0 ? true : false;
     }
 
     /**
@@ -57,7 +81,24 @@ public class MusicServiceImpl extends ServiceImpl<MusicMapper, Music>
      */
     @Override
     public boolean updateMusic(MusicUpdateBo musicUpdateBo) {
-        return false;
+
+        System.out.println( musicUpdateBo );
+        // 创建用于更新的UpdateWapper
+        UpdateWrapper<Music> updateWrapper = new UpdateWrapper<>();
+        // 设置需要更新的字段 和更新条件
+        updateWrapper.set("title",musicUpdateBo.getTitle())
+                .set("artist",musicUpdateBo.getArtist())
+                .set("album",musicUpdateBo.getAlbum())
+                .set("category_id",musicUpdateBo.getCategoryId())
+                .eq("music_id",musicUpdateBo.getMusicId());
+
+        System.out.println( "updateWrapper:" + updateWrapper );
+        int update = musicMapper.update(new Music(), updateWrapper);
+        System.out.println( "musicUpdateBo:" + musicUpdateBo );
+        // 因为这个方法需要将更新过后的数据返回 所以可以再次查询数据库
+        Music music = musicMapper.selectById(musicUpdateBo.getCategoryId());
+        return update > 0 ? true : false;
+
     }
 
     /**
@@ -68,7 +109,7 @@ public class MusicServiceImpl extends ServiceImpl<MusicMapper, Music>
      */
     @Override
     public boolean deleteMusic(Integer musicId) {
-        return false;
+        return musicMapper.deleteById( musicId ) > 0 ? true : false;
     }
 }
 
