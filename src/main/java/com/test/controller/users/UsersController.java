a4c8c29218ff2df08ac78e9ed9bb8e5cc8a3a99b
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-29 02:24:37 +0800

    update

--- src/main/java/com/test/controller/users/UsersController.java
+++ src/main/java/com/test/controller/users/UsersController.java
@@ -4,7 +4,7 @@ package com.test.controller.users;
 import com.test.bean.bo.UsersLoginBo;
 import com.test.bean.bo.UsersRegistBo;
 import com.test.bean.dto.BaseDTO;
-import com.test.bean.dto.DTO;
+import com.test.exception.ResultData;
 import com.test.service.UsersService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -30,54 +30,63 @@ public class UsersController {
     /**
      * 客户注册接口
      * post http://127.0.0.1:8001/api/users/regist
+     *
      * @param usersRegistBo 客户注册业务模型对象
      * @return 响应报文体
-     * */
+     */
     @PostMapping("/users/regist")
     @ApiOperation("客户注册接口")
-    public BaseDTO regist(UsersRegistBo usersRegistBo ){
+    public ResultData<Object> regist(UsersRegistBo usersRegistBo ){
         // 调用业务逻辑层 的 客户注册功能
         int result = usersService.regist( usersRegistBo );
         // 实例化 DTO 对象 => 封装响应报文体
-        BaseDTO dto = new BaseDTO();
+//        BaseDTO dto = new BaseDTO();
         if( result == 1 ){
-            dto.setCode( 200 );
-            dto.setMessage( "注册成功" );
+//            dto.setCode( 200 );
+//            dto.setMessage( "注册成功" );
+            return ResultData.success( "注册成功" );
         }else if( result == -1 ){
-            dto.setCode( 501 );
-            dto.setMessage( "短信验证码校验失败" );
+//            dto.setCode( 501 );
+//            dto.setMessage( "短信验证码校验失败" );
+            return ResultData.fail( 500,"短信验证码校验失败" );
         }else if( result == -2 ){
-            dto.setCode( 502 );
-            dto.setMessage( "该手机号码已注册" );
+//            dto.setCode( 502 );
+//            dto.setMessage( "该手机号码已注册" );
+            return ResultData.fail( 500,"该手机号码已注册" );
+        }else {
+            return null;
         }
-        return dto;
+//        return dto;
     }
 
     /**
      * 客户登录接口
      * post http://127.0.0.1:8001/api/users/login
+     *
      * @param usersLoginBo 客户登录业务模型对象
      * @return 响应报文体
-     * */
+     */
     @PostMapping("/users/login")
     @ApiOperation("客户登录接口")
-    public DTO login(UsersLoginBo usersLoginBo ){
+    public ResultData login(UsersLoginBo usersLoginBo ){
         // 调用业务逻辑层的 客户登录功能
         String token = usersService.login( usersLoginBo );
         // 实例化 DTO 对象，封装响应报文体
-        DTO dto = new DTO();
+//        DTO dto = new DTO();
         // 判断 登录是否成功
         if( token == null ){
             // 登录失败
-            dto.setCode( 401 );
-            dto.setMessage( "手机号或密码错误，请重新填写" );
+//            dto.setCode( 401 );
+//            dto.setMessage( "手机号或密码错误，请重新填写" );
+            return ResultData.fail(500,"请登录");
         }else{
             // 登录成功
-            dto.setCode( 200 );
-            dto.setMessage( "登录成功" );
-            dto.setData( token );
+//            dto.setCode( 200 );
+//            dto.setMessage( "登录成功" );
+//            dto.setData( token );
+            return ResultData.success(usersService.login( usersLoginBo ));
         }
-        return dto;
+
     }
 
 }
