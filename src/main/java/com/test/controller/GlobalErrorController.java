13d1bcecfd7d45ce88c3db21d3d52394e9a484b7
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-15 05:20:59 +0800

    update

--- src/main/java/com/test/controller/GlobalErrorController.java
+++ src/main/java/com/test/controller/GlobalErrorController.java
@@ -5,6 +5,12 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ * 显式处理 /error
+ * 不使用默认的错误处理机制, 即: "This application has no explicit mapping for /error, so you are seeing this as a fallback."
+ *
+ */
 @RestController
 public class GlobalErrorController implements ErrorController {
 
