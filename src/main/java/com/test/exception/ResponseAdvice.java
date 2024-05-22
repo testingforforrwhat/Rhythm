e068bc181e075c1637dfd5522f8dfb640a6581bc
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-22 16:13:44 +0800

    update

--- src/main/java/com/test/exception/ResponseAdvice.java
+++ src/main/java/com/test/exception/ResponseAdvice.java
@@ -21,7 +21,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  * ResponseBodyAdvice的作用：拦截Controller方法的返回值，统一处理返回值/响应体，一般用来统一返回格式，加解密，签名等等。
  *
  */
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.test.controller")
 public class ResponseAdvice implements ResponseBodyAdvice<Object> {
     @Autowired
     private ObjectMapper objectMapper;
