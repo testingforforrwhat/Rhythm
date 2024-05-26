505fe41e1209e789f8c2a39d40d0c8622df98559
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-27 02:48:38 +0800

    update

--- src/main/java/com/test/config/WebMVCConfig.java
+++ src/main/java/com/test/config/WebMVCConfig.java
@@ -6,7 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
+ *
  * 静态资源访问自定义配置
+ *
+ * 代码中配置的优先级高于application.properties配置的优先级
+ *
  */
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
