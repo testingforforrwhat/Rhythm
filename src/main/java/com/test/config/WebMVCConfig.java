a1e73eada9411d6ab08a7576d62c24e6b9e8f06c
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-27 01:47:26 +0800

    update aop

---
+++ src/main/java/com/test/config/WebMVCConfig.java
@@ -0,0 +1,14 @@
+package com.test.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+ 
+@Configuration
+public class WebMVCConfig extends WebMvcConfigurerAdapter {
+ 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
