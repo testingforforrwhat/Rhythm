93132d49c6e2c7e2520d759b0e51ccc3cf71c0c0
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-27 01:51:59 +0800

    update 静态资源访问自定义配置

--- src/main/java/com/test/config/WebMVCConfig.java
+++ src/main/java/com/test/config/WebMVCConfig.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
- 
+
+/**
+ * 静态资源访问自定义配置
+ */
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
  
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
