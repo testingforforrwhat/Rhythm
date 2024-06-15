ddb83afcf0590247f4154450a28bb6ef97316be8
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-06-16 02:41:01 +0800

    update

--- rhythm.sh
+++ rhythm.sh
@@ -16,3 +16,9 @@ cd /usr/local/zookeeper/bin
 ./zkServer.sh status
 ./zkServer.sh start
 
+cd /usr/local/kafka/bin/
+
+./kafka-server-start.sh /usr/local/kafka/config/server.properties &
+
+./kafka-topics.sh --list --bootstrap-server 127.0.0.1:9092
+
