22455c3d39270c7c3e7cadfc436b8bb56049042c
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-06-17 17:02:14 +0800

    update

--- src/main/java/com/test/controller/FileController.java
+++ src/main/java/com/test/controller/FileController.java
@@ -60,6 +60,46 @@ public class FileController {
         return null;
     }
 
+    @PostMapping("/upload/audioFile")
+    public Object uploadAudioFile( MultipartFile multipartFile ) throws IOException {
+
+        System.out.println( "参数名称 = " + multipartFile.getName() );
+        System.out.println( "文件类型 = " + multipartFile.getContentType() );
+        System.out.println( "原文件名 = " + multipartFile.getOriginalFilename() );
+        System.out.println( "文件大小 = " + multipartFile.getSize() );
+
+        // 判断 上传的文件类型 是否是音频文件
+        if( ! multipartFile.getContentType().startsWith("audio/") ){
+            System.out.println( "==> 上传的文件不是音频文件，上传失败" );
+            return null;
+        }
+
+        // 判断 上传的文件尺寸
+        if( multipartFile.getSize() > 20 * 1024 * 1024 ){
+            System.out.println( "==> 上传的文件最多20MB，上传失败" );
+            return null;
+        }
+
+        // 给 上传文件 生成服务器端 文件名
+        String filename = UUID.randomUUID().toString() +
+                multipartFile.getOriginalFilename().substring( multipartFile.getOriginalFilename().lastIndexOf(".") );
+
+
+        String filePath = ResourceUtils.getURL("classpath:").getPath() +
+                "static/uploads/" + filename;
+        // 绝对路径前面多了一个/ 去除
+        String fileNewPath = filePath.substring(1);
+        System.out.println("fileNewPath: " + fileNewPath);
+
+        // 指定 转移目标文件
+        File target = new File( filePath );
+
+        // 将 上传文件 从临时目录 转移到 目标文件夹
+        multipartFile.transferTo( target );
+
+        return null;
+    }
+
     @GetMapping("/download")
     public ResponseEntity<byte[]> download( String filename ) throws IOException {
 
