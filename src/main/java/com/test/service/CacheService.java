d37175a78d55a24ceb2853dcaab64ebe045c0290
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-06-18 16:20:24 +0800

    update

--- src/main/java/com/test/service/CacheService.java
+++ src/main/java/com/test/service/CacheService.java
@@ -1,4 +1,13 @@
 package com.test.service;
 
 public interface CacheService {
+
+    /**
+     * 删除缓存
+     *
+     * @param key
+     * @return
+     */
+     boolean deleteCache(String key);
+
 }
--- src/main/java/com/test/service/impl/CacheServiceImpl.java
+++ src/main/java/com/test/service/impl/CacheServiceImpl.java
@@ -3,4 +3,18 @@ package com.test.service.impl;
 import com.test.service.CacheService;
 
 public class CacheServiceImpl implements CacheService {
+
+
+    /**
+     * 删除缓存
+     *
+     * @param key
+     * @return
+     */
+    @Override
+    public boolean deleteCache(String key) {
+        return false;
+    }
+
+
 }
