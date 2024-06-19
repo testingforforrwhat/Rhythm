5268735f3ce119323a3b1800fcdc87135d6fcf5e
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-06-20 03:11:03 +0800

    update

--- src/main/java/com/test/handler/RestExceptionHandler.java
+++ src/main/java/com/test/handler/RestExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 @Slf4j
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -21,6 +23,18 @@ public class RestExceptionHandler {
     public ResultData<String> exception(Exception e) {
         log.error("全局异常信息 ex={}", e.getMessage(), e);
         return ResultData.fail(ReturnCode.RC500.getCode(),e.getMessage());
+    }
+
+    /**
+     * 默认sql异常处理。
+     * @param e_sql the e_sql
+     * @return ResultData
+     */
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResultData<String> exception(SQLException e_sql) {
+        log.error("sql异常信息 ex={}", e_sql.getMessage(), e_sql);
+        return ResultData.fail(ReturnCode.RC500.getCode(),e_sql.getMessage());
     }
 
 }
