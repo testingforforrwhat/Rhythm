9a16b6cd23d47d0ad33f11b6563c3da92ffd1f4a
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-06-20 16:20:30 +0800

    update 拦截请求并执行一些预处理或后处理逻辑: 捕获请求的方法信息

---
+++ src/main/java/com/test/interceptor/CustomInterceptor.java
@@ -0,0 +1,34 @@
+package com.test.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomInterceptor implements HandlerInterceptor {
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            String methodName = handlerMethod.getMethod().getName();
+            String className = handlerMethod.getBeanType().getName();
+            System.out.println("Handling method: " + className + "." + methodName);
+        }
+        return true; // 继续处理请求
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // 可以在这里执行一些处理逻辑，比如记录日志
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 请求处理完成后的逻辑
+    }
+}
