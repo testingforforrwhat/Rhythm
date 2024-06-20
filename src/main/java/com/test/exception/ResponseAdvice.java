64ebf5936b49d609ca675c1f035c058661f58489
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-06-20 18:17:40 +0800

    update 拦截请求并执行一些预处理或后处理逻辑: 捕获请求的方法信息

--- src/main/java/com/test/exception/ResponseAdvice.java
+++ src/main/java/com/test/exception/ResponseAdvice.java
@@ -60,14 +60,17 @@ public class ResponseAdvice implements ResponseBodyAdvice<Object> {
         if(o instanceof byte[] ){
             return objectMapper.writeValueAsBytes(ResultData.success(o));
         }
-        if(o == null ){
+        if(o == null ) {
             System.out.println("mediaType: " + mediaType);
             System.out.println("methodParameter: " + methodParameter);
             System.out.println("methodParameter.getMethod().getName(): " + methodParameter.getMethod().getName());
-            return objectMapper.writeValueAsString(ResultData.fail(204,"return null: "));
-        }
-        if(o == null && methodParameter.getMethod().getName() == "playAudio" ){
-            return objectMapper.writeValueAsString(ResultData.fail(204,"File does not exist: "));
+
+
+            if (o == null && methodParameter.getMethod().getName() == "playAudio") {
+                return objectMapper.writeValueAsString(ResultData.fail(204, "File does not exist: "));
+            }
+
+            return objectMapper.writeValueAsString(ResultData.fail(204, "return null: "));
         }
         return ResultData.success(o);
     }
