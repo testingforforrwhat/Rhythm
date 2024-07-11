a44d7d0f69f604fdb5029c07a50a127ddd4a6b68
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-07-11 17:07:03 +0800

    #enhancement
    #2 #3
    
    add /springSecurity/register

--- src/main/java/com/test/mapper/AdminMapper.java
+++ src/main/java/com/test/mapper/AdminMapper.java
@@ -2,6 +2,7 @@ package com.test.mapper;
 
 import com.test.bean.po.Admin;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author 23194
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @createDate 2024-06-27 02:34:06
 * @Entity com.test.bean.po.Admin
 */
+@Mapper
 public interface AdminMapper extends BaseMapper<Admin> {
 
     Admin getOneByName(String username);
