88527b55a85e51b95bfe37e24b320bde1a7e058c
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-07-15 19:32:41 +0800

    #enhancement
    #2 #3
    
    roleList
    
    注意避免全局性捕获所有异常（如 Exception.class），这可能会覆盖特定的异常处理（如 AccessDeniedException）

--- src/main/java/com/test/handler/RestExceptionHandler.java
+++ src/main/java/com/test/handler/RestExceptionHandler.java
@@ -31,12 +31,12 @@ public class RestExceptionHandler {
      * @param e the e
      * @return ResultData
      */
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResultData<String> exception(Exception e) {
-        log.error("全局异常信息 ex={}", e.getMessage(), e);
-        return ResultData.fail(ReturnCode.RC500.getCode(),e.getMessage());
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public ResultData<String> exception(Exception e) {
+//        log.error("全局异常信息 ex={}", e.getMessage(), e);
+//        return ResultData.fail(ReturnCode.RC500.getCode(),e.getMessage());
+//    }
 
     /**
      * 默认sql异常处理。
