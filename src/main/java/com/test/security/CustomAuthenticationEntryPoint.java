b53620202d2769bd4340e7a74b7d4a3cc62eb3ac
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-07-15 18:58:26 +0800

    #enhancement
    #2 #3
    
    roleList
    
    正确加载用户的权限信息并验证。使得 Spring Security 根据用户的具体权限（如角色）进行访问控制和授权

--- src/main/java/com/test/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/test/security/CustomAuthenticationEntryPoint.java
@@ -14,7 +14,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
 
-        System.out.println(" 401 自定义 AuthenticationEntryPoint 来处理未经认证的请求");
+        System.out.println(" 401 自定义 AuthenticationEntryPoint 来处理未经认证即未登录的请求");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);  // 401 Unauthorized
         response.getWriter().write("Authentication is required to access this resource.");  // 自定义消息
     }
