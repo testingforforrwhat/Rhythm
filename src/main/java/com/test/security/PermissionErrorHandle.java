bba001d67df63a488f478a08c69e9fdb1ff4696a
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-07-15 19:13:46 +0800

    #enhancement
    #2 #3
    
    roleList
    
    自定义 AuthenticationEntryPoint 来处理未经认证的请求。
    自定义 AccessDeniedHandler 来处理已认证但权限不足的请求

--- src/main/java/com/test/security/PermissionErrorHandle.java
+++ src/main/java/com/test/security/PermissionErrorHandle.java
@@ -15,6 +15,11 @@ public class PermissionErrorHandle implements AccessDeniedHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
         // 权限认证失败  重定向 到 权限错误提示页面
         System.out.println("该接口权限认证失败  重定向 到 权限错误提示页面");
-        response.sendRedirect("/index/error?message=Access Denied!&detail=You do not have permission to access this resource!");
+//        response.sendRedirect("/index/error?message=Access Denied!&detail=You do not have permission to access this resource!");
+
+        System.out.println(" 403 自定义 CustomAccessDeniedHandler 请求中包含有效的 JWT，但用户没有所需的 ADMIN 权限，权限检查失败");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);  // 403 Forbidden
+        response.getWriter().write("You do not have permission to access this resource.");  // 自定义消息
+
     }
 }
