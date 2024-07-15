b409dc2de25f1cd9460488231b075c5e44abed28
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-07-15 16:56:44 +0800

    #enhancement
    #2 #3
    
    roleList
    
    如果不在控制器方法上使用@PreAuthorize 或权限注解（如 @Secured 或 @RolesAllowed），
    方法默认将对所有经过身份认证的用户开放访问

--- src/main/java/com/test/controller/advertise/AdvertisementsController.java
+++ src/main/java/com/test/controller/advertise/AdvertisementsController.java
@@ -32,7 +32,15 @@ public class AdvertisementsController {
     // Spring Security 默认使用 ROLE_ 前缀来识别角色。
     // 因此，如果你的角色在数据库中存储的时候，已经包括了 ROLE_ 前缀，那么你在使用 @PreAuthorize 的时候也需要带上这个前缀，
     // 否则,无论是在数据库中还是在代码中，确保角色的命名保持一致。比如：ROLE_test 或者 test
-    @PreAuthorize("hasRole('test')")
+
+    /**
+     *
+     * 如果你不在控制器方法上使用 @PreAuthorize 或权限注解（如 @Secured 或 @RolesAllowed），
+     * 方法默认将对所有经过身份认证的用户开放访问。这意味着，只要用户成功通过认证（例如成功登录得到一个有效的 JWT），就可以访问该方法
+     * @param ad_id
+     * @return
+     */
+    @PreAuthorize("hasRole('用户')")
     @ResponseBody
     @ApiOperation("根据ad_id查询对应信息")
     @GetMapping("/ads/{ad_id}")
