f066069d4e9d4f14c47d990682322daa90bd3011
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-07-16 16:20:58 +0800

    #enhancement
    #2 #3
    
    roleList

--- src/main/java/com/test/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/test/security/CustomAccessDeniedHandler.java
@@ -14,6 +14,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
 
+        System.out.println(" 该接口权限认证失败  重定向 到 权限错误提示页面");
         System.out.println(" 403 自定义 CustomAccessDeniedHandler 请求中包含有效的 JWT，但用户没有所需的 ADMIN 权限，权限检查失败");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);  // 403 Forbidden
         response.getWriter().write("You do not have permission to access this resource.");  // 自定义消息
