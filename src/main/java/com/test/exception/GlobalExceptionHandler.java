ffe4f20ba3fe8c63ee5d17be9059ddaecded3084
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-08-05 22:48:52 +0800

    在使用 Spring 的全局异常处理器（例如 @ControllerAdvice）时，我们需要注意避免全局性捕获所有异常（如 Exception.class），这可能会覆盖特定的异常处理（如 AccessDeniedException）。

--- src/main/java/com/test/exception/GlobalExceptionHandler.java
+++ src/main/java/com/test/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.test.exception;
 
+import com.test.security.PermissionErrorHandle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *
@@ -18,12 +25,31 @@ import org.springframework.http.HttpStatus;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(AccessDeniedException.class)
-//    @ResponseStatus(HttpStatus.FORBIDDEN)
-//    public String handleAccessDeniedException(AccessDeniedException ex, Model model) {
-//        model.addAttribute("error", "You do not have permission to access this page.");
-//        return "error/403";
-//    }
+    @Autowired
+    private PermissionErrorHandle permissionErrorHandle;
+
+    /**
+     *
+     * 如果存在全局异常处理器，它可能会拦截并处理异常，从而导致自定义的 AccessDeniedHandler 未被调用。
+     *
+     * 我们可以采取以下策略：
+     *
+     * 确保 Spring Security 异常处理优先：确保 Spring Security 的异常处理优先于全局异常处理器。可以在全局异常处理器中排除 Spring Security 相关的异常。
+     *
+     * 手动调用 AccessDeniedHandler：在全局异常处理器中，如果捕获到 AccessDeniedException，可以手动调用自定义的 AccessDeniedHandler。
+     *
+     *
+     * @param request
+     * @param response
+     * @param ex
+     * @throws ServletException
+     * @throws IOException
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    public void handleAccessDeniedException(HttpServletRequest request, HttpServletResponse response, AccessDeniedException ex) throws ServletException, IOException {
+        // 手动调用自定义的 AccessDeniedHandler
+        permissionErrorHandle.handle(request, response, ex);
+    }
 
     @ExceptionHandler(InsufficientAuthenticationException.class)
     public String handleInsufficientAuthenticationException(InsufficientAuthenticationException ex, Model model) {
